f85d388b142a00cef783127034d954a2b6f82715
Kardee12 <emailclub72@gmail.com>
2022-08-09 16:02:02 -0700

    Initial Commit

---
+++ ImageRS/ImageRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRSApp.swift
+//  ImageRS
+//
+//  Created by Karthik Manishankar on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
